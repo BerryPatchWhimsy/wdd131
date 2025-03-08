f78df40e595e064549b5ee2670e3466375a1c5a4
BerryPatchWhimsy <berrypatchgirl@myyahoo.com>
2025-03-08 15:59:01 -0800

    index html CSS and JavaScript updated

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,32 +1,16 @@
 const currentyear = document.querySelector("#currentyear");
-const lastModified = document.querySelector("#lastModified");
 
 const today = new Date();
 
-full.innerHTML = `Today is <span class="highlight">${new Intl.DateTimeFormat(
-    "en-US",
-    {
-        dateStyle: "full"
-    }
-).format(today)}</span>`;
-worldfull.innerHTML = `UK: <span class="highlight">${new Intl.DateTimeFormat(
-    "en-UK",
-    {
-        dateStyle: "full"
-    }
-).format(today)}</span>`;
-short.innerHTML = `Short: <span class="highlight">${new Intl.DateTimeFormat(
-    "en-US",
-    {
-        dateStyle: "short"
-    }
-).format(today)}</span>`;
-medium.innerHTML = `Medium: <span class="highlight">${new Intl.DateTimeFormat(
-    "en-US",
-    {
-        dateStyle: "medium"
-    }
-).format(today)}</span>`;
+currentyear.innerHTML = `<span>${today.getFullYear()} </span>`;
 
-currentyear.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
+let lastModified = document.querySelector("#lastModified");
+
+lastModified.innerHTML = `Last modified: ${document.lastModified}`;
+// // getdates.js
+// function getFullYear() {
+//     const today = new Date();
+//     return today.toDateString();
+// }
+
 
